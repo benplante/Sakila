5f08d714bd3ee520f1e4f39c3db579a2da1f1916
Ben Plante <ben.plante123@gmail.com>
2020-07-22 16:14:35 -0400

    Actor MVC

---
+++ src/com/blockbuster/sakila/SuperController.java
@@ -0,0 +1,16 @@
+package com.blockbuster.sakila;
+
+import javax.swing.JFrame;
+
+public class SuperController extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public SuperController() {
+		this.setLocationRelativeTo(null);
+	}
+
+}
